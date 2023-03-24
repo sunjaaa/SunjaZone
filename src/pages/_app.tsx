0aa113c7d6c759dd403e719f3fc3125803025cbf
sunjaaa <senju5585@naver.com>
2023-03-24 13:52:35 +0900

    add : selection 글로벌 스타일 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,25 @@
+import type { AppProps } from "next/app";
+import { css, Global } from "@emotion/react";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Layout from "@/Layout/Layout";
-import type { AppProps } from "next/app";
+import { content } from "@/constants";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
+const selectionStyle = css`
+  ::selection {
+    background-color: ${content.primary.common};
+  }
+`;
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Global styles={selectionStyle} />
     <Layout
       main={<Component {...pageProps} />}
       header={<Header />}
       footer={<Footer />}
     />
-  );
-}
+  </>
+);
+
+export default App;
