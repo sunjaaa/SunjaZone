6c0441edd3aa6dabb8a3fad5eda62889042b9772
sunjaaa <senju5585@naver.com>
2023-03-24 13:16:39 +0900

    add : 배너 이미지 추가

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import styled from "@emotion/styled";
+import Icon from "../Icon/Icon";
 
 const Banner = () => {
   return (
     <Container>
-      <Title>
-        안녕하세요, <br />
-        FrontEnd Engineer 이선재입니다.
-        <Description>
-          생각없이 복종하며 졸음에 빠진 삶을 이어나가는 '코더'가 아닌,
-          <br />
-          자신의 코드와 조직을 지배하는 '개발자'가 목표입니다.
-        </Description>
-      </Title>
+      <div>
+        <h1>
+          안녕하세요, <br />
+          FrontEnd Engineer 이선재입니다.
+          <Description>
+            생각없이 복종하며 졸음에 빠진 삶을 이어나가는 '코더'가 아닌,
+            <br />
+            자신의 코드와 조직을 지배하는 '개발자'가 목표입니다.
+          </Description>
+        </h1>
+      </div>
+      <div>
+        <Icon icon="devJeans" size={200} />
+      </div>
     </Container>
   );
 };
@@ -24,8 +30,8 @@ const Container = styled.div`
   width: 100%;
   margin-top: 5%;
   margin-bottom: 5%;
+  justify-content: space-around;
 `;
-const Title = styled.h1``;
 
 const Description = styled.p`
   font-size: 16px;
