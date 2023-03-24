9d1258b9d24cdd1d6bd283c0f5282f2635cad054
sunjaaa <senju5585@naver.com>
2023-03-24 13:54:45 +0900

    add : image/next 사용시 필요한 domains 추가 및 svg파일 허용

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["img.shields.io"],
+    dangerouslyAllowSVG: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
