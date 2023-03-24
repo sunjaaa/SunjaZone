58a50e49ed3b153d3163623f4a83e5ebc57bd59b
sunjaaa <senju5585@naver.com>
2023-03-24 13:55:38 +0900

    fix : badge 오타 수정

--- src/constants/badge.ts
+++ src/constants/badge.ts
@@ -4,6 +4,6 @@ export const BADGE = {
   REACT: `${base}-React-61DAFB?logo=react&logoColor=black&style=flat"`,
   REACT_NATIVE: `${base}-React%20Native-61DAFB?logo=react&logoColor=black&style=flat"`,
   JAVASCRIPT: `${base}-JavaScript-F7DF1E?logo=javascript&logoColor=black&style=flat"`,
-  TYPESCRIPT: `${base}-TypeScript-007ACC?logo=typescript&logoColor=white&width=100&height=20&style=flat"`,
+  TYPESCRIPT: `${base}-TypeScript-007ACC?logo=typescript&logoColor=white&style=flat"`,
   EXPRESS: `${base}-Express.js-000000?logo=express&logoColor=white&style=flat"`,
 };
