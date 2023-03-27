63c0a072bf3c2df9b7a74477df4d0822c956ff04
sunjaaa <senju5585@naver.com>
2023-03-27 14:44:49 +0900

    design: px을 rem으로 변경 및 상세 디자인 변경

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -5,20 +5,22 @@ import Icon from "../Icon/Icon";
 const Banner = () => {
   return (
     <Container>
-      <div>
-        <h1>
-          안녕하세요, <br />
-          FrontEnd Engineer 이선재입니다.
-          <Description>
-            생각없이 복종하며 졸음에 빠진 삶을 이어나가는 '코더'가 아닌,
-            <br />
-            자신의 코드와 조직을 지배하는 '개발자'가 목표입니다.
-          </Description>
-        </h1>
-      </div>
-      <div>
-        <Icon icon="devJeans" size={200} />
-      </div>
+      <Wrapper>
+        <div>
+          <h1>
+            안녕하세요, <br />
+            FrontEnd Engineer 이선재입니다.
+            <Description>
+              생각없이 복종하며 졸음에 빠진 삶을 이어나가는 '코더'가 아닌,
+              <br />
+              자신의 코드와 조직을 지배하는 '개발자'가 목표입니다.
+            </Description>
+          </h1>
+        </div>
+        <div>
+          <Icon icon="devJeans" size={200} />
+        </div>
+      </Wrapper>
     </Container>
   );
 };
@@ -28,9 +30,13 @@ export default Banner;
 const Container = styled.div`
   display: flex;
   width: 100%;
-  margin-top: 5%;
-  margin-bottom: 5%;
-  justify-content: space-around;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin: 65px 16px 65px 16px;
 `;
 
 const Description = styled.p`
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,22 +1,23 @@
 import styled from "@emotion/styled";
 import React from "react";
-// import styled from "styled-components";
-import Banner from "../components/Banner/Banner";
-import CustomItem from "../components/CustomItem/CustomItem";
-import Icon from "../components/Icon/Icon";
+import Banner from "@/components/Banner/Banner";
+import CustomItem from "@/components/CustomItem/CustomItem";
+import Icon from "@/components/Icon/Icon";
 
 const Main = () => {
   return (
     <Container>
-      <Banner />
-      <div>Main</div>
-      <CustomItem
-        icon={<Icon icon="cloudfare" size={20} />}
-        title="Whatssub-Lite"
-        subtitle="Whatssub Internships"
-        description="React-Native를 활용한 애플리케이션 개발"
-        duration="Nov 2022 ~ Jan 2023"
-      />
+      <Wrapper>
+        <Banner />
+        <div>Main</div>
+        <CustomItem
+          icon={<Icon icon="cloudfare" size={20} />}
+          title="Whatssub-Lite"
+          subtitle="Whatssub Internships"
+          description="React-Native를 활용한 애플리케이션 개발"
+          duration="Nov 2022 ~ Jan 2023"
+        />
+      </Wrapper>
     </Container>
   );
 };
@@ -25,9 +26,14 @@ export default Main;
 
 const Container = styled.div`
   display: flex;
+  flex: 1;
+  width: 100dvw;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
   flex-direction: column;
   align-items: center;
-  flex: 1;
-  margin-left: 28%;
-  margin-right: 28%;
+  width: 50rem;
 `;
