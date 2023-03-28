aa0762c685bacd684c11925acc8171d7f9b8f83f
sunjaaa <senju5585@naver.com>
2023-03-28 17:33:38 +0900

    feat: nextjs, nestjs, docker, mariadb 뱃지 추가

--- src/constants/badge.ts
+++ src/constants/badge.ts
@@ -1,9 +1,13 @@
 const base = "https://img.shields.io/badge/";
 
 export const BADGE = {
-  REACT: `${base}-React-61DAFB?logo=react&logoColor=black&style=flat"`,
-  REACT_NATIVE: `${base}-React%20Native-61DAFB?logo=react&logoColor=black&style=flat"`,
-  JAVASCRIPT: `${base}-JavaScript-F7DF1E?logo=javascript&logoColor=black&style=flat"`,
-  TYPESCRIPT: `${base}-TypeScript-007ACC?logo=typescript&logoColor=white&style=flat"`,
+  REACT: `${base}-React-61DAFB?logo=react&logoColor=black&style=flat`,
+  REACT_NATIVE: `${base}-React%20Native-61DAFB?logo=react&logoColor=black&style=flat`,
+  JAVASCRIPT: `${base}-JavaScript-F7DF1E?logo=javascript&logoColor=black&style=flat`,
+  TYPESCRIPT: `${base}-TypeScript-007ACC?logo=typescript&logoColor=white&style=flat`,
   EXPRESS: `${base}-Express.js-000000?logo=express&logoColor=white&style=flat"`,
+  NEXTJS: `${base}-Next.js-000000?logo=next.js&logoColor=white&style=flat`,
+  NESTJS: `${base}-Nest.js-E0234E?logo=nestjs&logoColor=white&style=flat`,
+  MARIADB: `${base}-MariaDB-003545?logo=mariadb&logoColor=white&style=flat`,
+  DOCKER: `${base}-Docker-2496ED?logo=docker&logoColor=white&style=flat`,
 };
