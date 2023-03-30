3a22f949123398b2610369c7585f3fcf2d133944
sunjaaa <senju5585@naver.com>
2023-03-30 17:35:00 +0900

    refact: 배너 이미지 최적화를 위한 리팩토링

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -17,13 +17,29 @@ const Icon = ({ icon, size }: Props) => {
       .catch((err) => console.error(err));
   }, [icon]);
 
-  return (
-    <>
-      {imageSrc && (
-        <Image src={imageSrc} alt={icon} width={size} height={size} />
-      )}
-    </>
-  );
+  if (icon === "devJeans") {
+    return (
+      <>
+        {imageSrc && (
+          <Image
+            width={size}
+            height={size}
+            src="/_next/static/media/devJeans.3aa990f2.png"
+            alt="데브진스를 사용한 이미지 배너입니다!"
+            priority={true}
+          />
+        )}
+      </>
+    );
+  } else {
+    return (
+      <>
+        {imageSrc && (
+          <Image src={imageSrc} alt={icon} width={size} height={size} />
+        )}
+      </>
+    );
+  }
 };
 
 export default Icon;
