c97afb343d1e1f89b19c7ddb2099106fac18e37f
sunjaaa <senju5585@naver.com>
2023-03-30 17:18:30 +0900

    feat: 재사용 list컴포넌트인 FlatList컴포넌트 추가

--- src/components/FlatList/index.tsx
+++ src/components/FlatList/index.tsx
@@ -1,27 +1,48 @@
 import React from "react";
+import styled from "@emotion/styled";
 
-interface FlatListProps {
-  data: any;
-  renderItem: (item: any) => React.ReactNode;
+interface FlatListProps<T> {
+  data: T[];
+  keyExtractor: (item: T) => string;
+  renderItem: ({ item, index }: { item: T; index: number }) => JSX.Element;
   style?: React.CSSProperties;
   containerStyle?: React.CSSProperties;
+  ListHeaderComponent?: React.ReactNode;
+  ListFooterComponent?: React.ReactNode;
+  ItemSeparatorComponent?: React.ReactNode;
+  horizontal?: boolean;
 }
 
-const FlatList = ({
+const FlatList = <T,>({
   data,
+  keyExtractor,
   renderItem,
   style,
+  ListHeaderComponent,
+  ListFooterComponent,
+  ItemSeparatorComponent,
   containerStyle,
-}: FlatListProps) => {
+  horizontal = false,
+}: FlatListProps<T>) => {
   return (
-    <div style={containerStyle}>
-      {data.map((item: any, index: number) => (
-        <div key={item.id} style={style}>
-          {renderItem({ item, index })}
-        </div>
-      ))}
+    <div>
+      {ListHeaderComponent}
+      <Container style={containerStyle} horizontal={horizontal}>
+        {data.map((item: T, index: number) => (
+          <div key={keyExtractor(item)} style={style}>
+            {renderItem({ item, index })}
+            {ItemSeparatorComponent}
+          </div>
+        ))}
+      </Container>
+      {ListFooterComponent}
     </div>
   );
 };
 
 export default FlatList;
+
+const Container = styled.div<{ horizontal: boolean }>`
+  display: flex;
+  flex-direction: ${(props) => (props.horizontal ? "row" : "column")};
+`;
